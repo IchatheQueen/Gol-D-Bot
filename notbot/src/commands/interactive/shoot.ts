228383e9dff8fb8e611438ff73fdf755bd902b17
sadbitch <ellaspecter2020@gmail.com>
2025-12-07 21:47:11 -0800

    Polish rifle shoot output with total summary

--- notbot/src/commands/interactive/shoot.ts
+++ notbot/src/commands/interactive/shoot.ts
@@ -164,6 +164,8 @@ const command: Command = {
                 loopResults.push(`💵 ${formatBigNumber(loopStolen)} has been moved to ${attackerTag}'s account and 💵 ${formatBigNumber(loopDestroyed)} disintegrated`);
             }
 
+            loopResults.push(`\n**TOTAL:** 💵 ${formatBigNumber(totalStolen)} stolen | 💵 ${formatBigNumber(totalDamage)} destroyed`);
+
             resultMessage = loopResults.join('\n');
         }
         else if (weaponId === 'crossbow') {
